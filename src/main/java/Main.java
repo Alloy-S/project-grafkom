a607cc4ebffac688066ba8eda6ac8d5439f63d71
Sadrian <adriansteven92@gmail.com>
2023-04-13 11:01:57 +0700

    bersih"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,16 +17,11 @@ public class Main {
     private Window window =
             new Window
     (800,800,"Hello World");
-    private ArrayList<Object> objects
-            = new ArrayList<>();
-    private ArrayList<Object> objectsRectangle
-            = new ArrayList<>();
-
-    private ArrayList<Object> objectsPointsControl
-            = new ArrayList<>();
+    private ArrayList<Object> dartMonkey= new ArrayList<>();
+    private ArrayList<Object> ninjaMonkey= new ArrayList<>();
+    private ArrayList<Object> engineerMonkey= new ArrayList<>();
 
     private MouseInput mouseInput;
-    int countDegree = 0;
     Projection projection = new Projection(window.getWidth(),window.getHeight());
     Camera camera = new Camera();
     public void init(){
@@ -36,197 +31,12 @@ public class Main {
         camera.setPosition(0,0,1.7f);
         camera.setRotation((float)Math.toRadians(0.0f),(float)Math.toRadians(30.0f));
         //code
-//        objects.add(new Object2d(
-//            Arrays.asList(
-//                //shaderFile lokasi menyesuaikan objectnya
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.vert"
-//                , GL_VERTEX_SHADER),
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.frag"
-//                , GL_FRAGMENT_SHADER)
-//            ),
-//            new ArrayList<>(
-//                List.of(
-//                    new Vector3f(0.0f,0.5f,0.0f),
-//                    new Vector3f(-0.5f,-0.5f,0.0f),
-//                    new Vector3f(0.5f,-0.5f,0.0f)
-//                )
-//            ),
-//            new Vector4f(0.0f,1.0f,1.0f,1.0f)
-//        ));
-//        objects.add(new Object(
-//            Arrays.asList(
-//                //shaderFile lokasi menyesuaikan objectnya
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/" +
-//                    "sceneWithVerticesColor.vert"
-//                        , GL_VERTEX_SHADER),
-//                new ShaderProgram.ShaderModuleData
-//                    ("resources/shaders/" +
-//                    "sceneWithVerticesColor.frag"
-//                            , GL_FRAGMENT_SHADER)
-//        ),
-//        new ArrayList<>(
-//                List.of(
-//                    new Vector3f(0.0f,0.5f,0.0f),
-//                    new Vector3f(-0.5f,-0.5f,0.0f),
-//                    new Vector3f(0.5f,-0.5f,0.0f)
-//                )
-//            ),
-//        new ArrayList<>(
-//            List.of(
-//                new Vector3f(1.0f,0.0f,0.0f),
-//                new Vector3f(0.0f,1.0f,0.0f),
-//                new Vector3f(0.0f,0.0f,1.0f)
-//            )
-//        )
-//        ));
-//        objectsRectangle.add(new Rectangle(
-//            Arrays.asList(
-//                //shaderFile lokasi menyesuaikan objectnya
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.vert"
-//                , GL_VERTEX_SHADER),
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.frag"
-//                , GL_FRAGMENT_SHADER)
-//            ),
-//            new ArrayList<>(
-//                List.of(
-//                    new Vector3f(0.0f,0.0f,0.0f),
-//                    new Vector3f(0.5f,0.0f,0.0f),
-//                    new Vector3f(0.0f,0.5f,0.0f),
-//                    new Vector3f( 0.5f,0.5f,0.0f)
-//                )
-//            ),
-//            new Vector4f(0.0f,1.0f,1.0f,1.0f),
-//            Arrays.asList(0,1,2,1,2,3)
-//
-//        ));
-//        objectsPointsControl.add(new Object(
-//            Arrays.asList(
-//                //shaderFile lokasi menyesuaikan objectnya
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.vert"
-//                , GL_VERTEX_SHADER),
-//                new ShaderProgram.ShaderModuleData
-//                ("resources/shaders/scene.frag"
-//                , GL_FRAGMENT_SHADER)
-//            ),
-//            new ArrayList<>(),
-//            new Vector4f(0.0f,1.0f,1.0f,1.0f)
-//        ));
-        objects.add(new Sphere(
-                Arrays.asList(
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
-                ),
-                new ArrayList<>(),
-                new Vector4f(0.0f,1.0f,0.0f,1.0f),
-                Arrays.asList(0.0f,0.0f,0.0f),
-                0.125f,
-                0.125f,
-                0.125f,
-                36,
-                18
-        ));
-//        objects.get(0).translateObject(0.5f,0.0f,0.0f);
-        objects.get(0).scaleObject(1f,1f,1f);
 
-        objects.get(0).getChildObject().add(new Sphere(
-                Arrays.asList(
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
-                ),
-                new ArrayList<>(),
-                new Vector4f(0.0f,1.0f,0.0f,1.0f),
-                Arrays.asList(0.0f,0.0f,0.0f),
-                0.125f,
-                0.125f,
-                0.125f,
-                36,
-                18
-        ));
-        objects.get(0).getChildObject().get(0).translateObject(0.25f,0.0f,0.0f);
-//        objects.get(0).getChildObject().get(0).setCenterPoint(Arrays.asList(0.25f,0.0f,0.0f));
 
-        objects.get(0).getChildObject().add(new Sphere(
-                Arrays.asList(
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
-                ),
-                new ArrayList<>(),
-                new Vector4f(0.0f,1.0f,0.0f,1.0f),
-                Arrays.asList(0.0f,0.0f,0.0f),
-                0.125f,
-                0.125f,
-                0.125f,
-                36,
-                18
-        ));
-        objects.get(0).getChildObject().get(1).translateObject(0.5f,0.0f,0.0f);
-//        objects.get(0).getChildObject().get(1).setCenterPoint(Arrays.asList(0.5f,0.0f,0.0f));
-
-        objects.get(0).getChildObject().get(1).getChildObject().add(new Sphere(
-                Arrays.asList(
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER),
-                        new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)
-                ),
-                new ArrayList<>(),
-                new Vector4f(0.0f,1.0f,0.0f,1.0f),
-                Arrays.asList(0.0f,0.0f,0.0f),
-                0.125f,
-                0.125f,
-                0.125f,
-                36,
-                18
-        ));
-        objects.get(0).getChildObject().get(1).getChildObject().get(0).scaleObject(0.5f,0.5f,0.5f);
-        objects.get(0).getChildObject().get(1).getChildObject().get(0).translateObject(0.5f,-0.1f,0.0f);
-//        objects.get(0).getChildObject().get(1).getChildObject().get(0).setCenterPoint(Arrays.asList(0.5f,-0.1f,0.0f));
     }
     public void input(){
         if (window.isKeyPressed(GLFW_KEY_W)) {
-            countDegree++;
-            //rotasi terhadap matahari
-            objects.get(0).rotateObject((float) Math.toRadians(0.5f),0.0f,0.0f,1.0f);
-            for(Object child:objects.get(0).getChildObject()){
-                List<Float> temp = new ArrayList<>(child.getCenterPoint());
-                //rotasi terhadap sumbu masing-masing planet
-                child.translateObject(temp.get(0)*-1,temp.get(1)*-1,temp.get(2)*-1);
-                child.rotateObject((float) Math.toRadians(0.5f),0.0f,0.0f,1.0f);
-                child.translateObject(temp.get(0)*1,temp.get(1)*1,temp.get(2)*1);
-                for(Object y:objects.get(0).getChildObject().get(1).getChildObject()){
-                    //rotasi terhadap bumi
-                    List<Float> temp1 = new ArrayList<>(objects.get(0).getChildObject().get(1).getCenterPoint());
-                    y.translateObject(temp1.get(0)*-1,temp1.get(1)*-1,temp1.get(2)*-1);
-                    y.rotateObject((float) Math.toRadians(0.5f),0.0f,0.0f,1.0f);
-                    y.translateObject(temp1.get(0)*1,temp1.get(1)*1,temp1.get(2)*1);
-                    //rotasi terhadap sumbunya sendiri
-                    temp1 = new ArrayList<>(objects.get(0).getChildObject().get(1).getChildObject().get(0).getCenterPoint());
-                    y.translateObject(temp1.get(0)*-1,temp1.get(1)*-1,temp1.get(2)*-1);
-                    y.rotateObject((float) Math.toRadians(0.5f),0.0f,0.0f,1.0f);
-                    y.translateObject(temp1.get(0)*1,temp1.get(1)*1,temp1.get(2)*1);
-                }
-                child.rotateObject((float) Math.toRadians(0.5f),0.0f,0.0f,1.0f);
-            }
         }
-        if(mouseInput.isLeftButtonPressed()){
-            Vector2f pos = mouseInput.getCurrentPos();
-//            System.out.println("x : "+pos.x+" y : "+pos.y);
-            pos.x = (pos.x - (window.getWidth())/2.0f) /
-                    (window.getWidth()/2.0f);
-            pos.y = (pos.y - (window.getHeight())/2.0f) /
-                    (-window.getHeight()/2.0f);
-            //System.out.println("x : "+pos.x+" y : "+pos.y);
-
-            if((!(pos.x > 1 || pos.x < -0.97) && !(pos.y > 0.97 || pos.y < -1))){
-                System.out.println("x : "+pos.x+" y : "+pos.y);
-//                objectsPointsControl.get(0).addVertices(new Vector3f(pos.x,pos.y,0));
-            }
-        }
-
     }
     public void loop(){
         while (window.isOpen()) {
@@ -238,15 +48,9 @@ public class Main {
             input();
 
             //code
-            for(Object object: objects){
+            for(Object object: dartMonkey){
                 object.draw(camera,projection);
             }
-//            for(Object object: objectsRectangle){
-//                object.draw();
-//            }
-//            for(Object object: objectsPointsControl){
-//                object.drawLine();
-//            }
 
             // Restore state
             glDisableVertexAttribArray(0);

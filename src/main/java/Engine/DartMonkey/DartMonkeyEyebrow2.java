95583287093473dbd9d5a18a924af42c1f8ff01d
Sadrian <adriansteven92@gmail.com>
2023-04-15 17:24:00 +0700

    ALIS OMAIGAT

---
+++ src/main/java/Engine/DartMonkey/DartMonkeyEyebrow2.java
@@ -0,0 +1,137 @@
+package Engine.DartMonkey;
+
+import Engine.Object;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+
+public class DartMonkeyEyebrow2 extends Object {
+
+    float radiusX, radiusY, radiusZ;
+    int sectorCount, stackCount;
+    float offsetX, offsetY, offsetZ;
+    public DartMonkeyEyebrow2(List<ShaderModuleData> shaderModuleDataList, List<Vector3f> vertices, Vector4f color) {
+        super(shaderModuleDataList, vertices, color);
+        vertices.clear();
+
+        radiusX = 0.15f;
+        radiusY = 0.09f;
+        radiusZ = 0.08f;
+        sectorCount = 80;
+        stackCount = 80;
+        generate();
+        setupVAOVBO();
+        offsetX = 0.19f;
+        offsetY = 0.29f;
+        offsetZ = 0.2f;
+        rotateObject(0.1f,0.0f,0.0f,1.0f);
+        translateObject(offsetX, offsetY, offsetZ);
+    }
+
+    public void generate(){
+        vertices.clear();
+        Vector3f temp = new Vector3f();
+        ArrayList<Vector3f> tempVertices = new ArrayList<>();
+
+        //TITIK 1
+        temp.x = centerPoint.get(0) - radiusX / 2.0f;
+        temp.y = centerPoint.get(1) + radiusY / 2.0f;
+        temp.z = centerPoint.get(2) - radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 2
+        temp.x = centerPoint.get(0) + radiusX / 2.0f;
+        temp.y = centerPoint.get(1) + radiusY / 2.0f;
+        temp.z = centerPoint.get(2) - radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 3
+        temp.x = centerPoint.get(0) + radiusX / 2.0f;
+        temp.y = centerPoint.get(1) - radiusY / 2.0f;
+        temp.z = centerPoint.get(2) - radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 4
+        temp.x = centerPoint.get(0) - radiusX / 2.0f;
+        temp.y = centerPoint.get(1) - radiusY / 2.0f;
+        temp.z = centerPoint.get(2) - radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 5
+        temp.x = centerPoint.get(0) - radiusX / 2.0f;
+        temp.y = centerPoint.get(1) + radiusY / 2.0f;
+        temp.z = centerPoint.get(2) + radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 6
+        temp.x = centerPoint.get(0) + radiusX / 2.0f;
+        temp.y = centerPoint.get(1) + radiusY / 2.0f;
+        temp.z = centerPoint.get(2) + radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 7
+        temp.x = centerPoint.get(0) + radiusX / 2.0f;
+        temp.y = centerPoint.get(1) - radiusY / 2.0f;
+        temp.z = centerPoint.get(2) + radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+        //TITIK 8
+        temp.x = centerPoint.get(0) - radiusX / 2.0f;
+        temp.y = centerPoint.get(1) - radiusY / 2.0f;
+        temp.z = centerPoint.get(2) + radiusZ / 2.0f;
+        tempVertices.add(temp);
+        temp = new Vector3f();
+
+        vertices.clear();
+        //kotak yg sisi belakang
+        vertices.add(tempVertices.get(0));
+        vertices.add(tempVertices.get(1));
+        vertices.add(tempVertices.get(2));
+        vertices.add(tempVertices.get(3));
+        //kotak yg sisi depan
+        vertices.add(tempVertices.get(4));
+        vertices.add(tempVertices.get(5));
+        vertices.add(tempVertices.get(6));
+        vertices.add(tempVertices.get(7));
+        //kotak yg sisi kiri
+        vertices.add(tempVertices.get(0));
+        vertices.add(tempVertices.get(4));
+        vertices.add(tempVertices.get(7));
+        vertices.add(tempVertices.get(3));
+        //kotak yg sisi kanan
+        vertices.add(tempVertices.get(1));
+        vertices.add(tempVertices.get(5));
+        vertices.add(tempVertices.get(6));
+        vertices.add(tempVertices.get(2));
+        //kotak yg sisi atas
+        vertices.add(tempVertices.get(0));
+        vertices.add(tempVertices.get(1));
+        vertices.add(tempVertices.get(5));
+        vertices.add(tempVertices.get(4));
+        //kotak yg sisi bawah
+        vertices.add(tempVertices.get(3));
+        vertices.add(tempVertices.get(2));
+        vertices.add(tempVertices.get(7));
+        vertices.add(tempVertices.get(6));
+
+        // bikin anak di sini
+        List<Object> children = new ArrayList<>();
+
+        setChildObject(children);
+    }
+    public void rotateObject(Float degree, Float x,Float y,Float z) {
+        model = new Matrix4f().rotate(degree, x, y, z).mul(new Matrix4f(model));
+        updateCenterPoint();
+        for (Object child : childObject) {
+            child.rotateObject(degree, x, y, z);
+        }
+    }
+}
+
--- src/main/java/Engine/DartMonkey/DartMonkeyFace.java
+++ src/main/java/Engine/DartMonkey/DartMonkeyFace.java
@@ -86,6 +86,30 @@ public class DartMonkeyFace extends Object {
                 new ArrayList<>(),
                 new Vector4f(0.77f,0.77f,0.77f,1.0f)
         ));
+        children.add(new DartMonkeyEyebrow1(
+                Arrays.asList(
+                        new ShaderModuleData
+                                ("resources/shaders/scene.vert"
+                                        , GL_VERTEX_SHADER),
+                        new ShaderModuleData
+                                ("resources/shaders/scene.frag"
+                                        , GL_FRAGMENT_SHADER)
+                ),
+                new ArrayList<>(),
+                new Vector4f(0.3f,0.15f,0.08f,1.0f)
+        ));
+        children.add(new DartMonkeyEyebrow2(
+                Arrays.asList(
+                        new ShaderModuleData
+                                ("resources/shaders/scene.vert"
+                                        , GL_VERTEX_SHADER),
+                        new ShaderModuleData
+                                ("resources/shaders/scene.frag"
+                                        , GL_FRAGMENT_SHADER)
+                ),
+                new ArrayList<>(),
+                new Vector4f(0.3f,0.15f,0.08f,1.0f)
+        ));
 
 
         setChildObject(children);
